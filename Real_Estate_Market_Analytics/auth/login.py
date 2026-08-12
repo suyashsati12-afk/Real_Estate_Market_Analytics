2522d30b5421c0b67cec0df52d3021afacf1eaa6
suyashsati12-afk <suyashsati12@gmail.com>
2026-08-12 19:58:28 +0530

    Update login.py

--- Real_Estate_Market_Analytics/auth/login.py
+++ Real_Estate_Market_Analytics/auth/login.py
@@ -1,9 +1,17 @@
 import sqlite3
 import bcrypt
+import os
+
+# Project root directory
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Database path
+DB_PATH = os.path.join(BASE_DIR, "database", "users.db")
+
 
 def login_user(username, password):
 
-    conn = sqlite3.connect("database/users.db")
+    conn = sqlite3.connect(DB_PATH)
     cursor = conn.cursor()
 
     cursor.execute(
